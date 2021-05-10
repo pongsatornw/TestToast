1fdfcf41aa21964b4cb52cfc8b5a2dddf8d76a40
Pongsatorn Wangriatisuk <pongsatorn@lmwn.com>
2021-05-10 12:36:21 +0700

    Test

--- TestLib/src/main/java/com/yggdrasil/testlib/MainActivity2.kt
+++ TestLib/src/main/java/com/yggdrasil/testlib/MainActivity2.kt
@@ -1,7 +1,12 @@
 package com.yggdrasil.testlib
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2: AppCompatActivity() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
--- TestLib/src/main/java/com/yggdrasil/testlib/TestToast.kt
+++ TestLib/src/main/java/com/yggdrasil/testlib/TestToast.kt
@@ -1,11 +1,13 @@
 package com.yggdrasil.testlib
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 
 object TestToast {
 
     fun create(context: Context, text: String) {
+        context.startActivity(Intent(context, MainActivity2::class.java))
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 }
